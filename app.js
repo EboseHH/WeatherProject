78a1fb2d03df533c07c531b6c5821a34d392b81d
Ebose <o.ebosetale@gmail.com>
2023-09-29 23:39:08 +0100

    refatored app.js

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
+// import dependencies
+require("dotenv").config();
 const { query } = require("express");
 const express = require("express");
-const { STATUS_CODES } = require("http");
-const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// return index.ejs on loading homepage
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.render("pages/index");
+});
 
-  app.post("/", function (req, res) {
-    const query = req.body.cityName;
-    const apiKey = "807340b847e45b1f0394804aac572866";
-    const url =
-      "https://api.openweathermap.org/data/2.5/weather?appid=" +
-      apiKey +
-      "&units=metric&q=" +
-      query;
-    https.get(url, function (response) {
-      response.on("data", function (data) {
-        const weatherData = JSON.parse(data);
-        const temp = weatherData.main.temp;
-        const weatherDescription = weatherData.weather[0].description;
-        const icon = weatherData.weather[0].icon;
+// using async function to wait for an operation to complete
+app.post("/", async function (req, res) {
+  // get user's input using body parser.
+  const query1 = req.body.cityName1;
+  const query2 = req.body.cityName2;
+  // declare api key
+  const apiKey = process.env.OPEN_WEATHER_API_KEY;
+  // url is the api endpoint which gives us access to the openweather data
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?appid=" +
+    apiKey +
+    "&units=metric&q=";
+  // await says wait for the operation, fetch returns the data that corresponds with the city name inputed by the user
+  const response1 = await fetch(url + query1);
+  // convert repsonse to javascript object using await because this happens asynchronously
+  const data1 = await response1.json();
+  // repeat for second input
+  const response2 = await fetch(url + query2);
+  const data2 = await response2.json();
 
-        res.write("<p>The weather is currently " + weatherDescription + "</p>");
-        res.write(
-          "<h1>The temperature in " +
-            query +
-            " is " +
-            temp +
-            " degree celsius</h1>"
-        );
-        res.send();
-      });
-    });
+  const cities = [
+    {
+      name: data1.name,
+      temperature: data1.main.temp,
+      description: data1.weather[0].description,
+      icon: data1.weather[0].icon,
+    },
+    {
+      name: data2.name,
+      temperature: data2.main.temp,
+      description: data2.weather[0].description,
+      icon: data2.weather[0].icon,
+    },
+  ];
+
+  res.render("pages/results", {
+    cities: cities,
   });
 });
 
